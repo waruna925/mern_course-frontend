656839c586f80fb370196866df66742ff4241f70
waruna925 <warunasuraweera925@gmail.com>
2026-05-25 14:57:25 +0530

    pagination 🖋️

---
+++ src/assets/sampleProducts.js
@@ -0,0 +1,38 @@
+export const sampleProducts=[
+    {
+        "_id": "696aa5514a8c1789dbda1d1c",
+        "productId": "PRD-001",
+        "name": " BT Mouse",
+        "altNames": [
+            "Bluetooth Mouse",
+            "Cordless Mouse"
+        ],
+        "description": "Ergonomic wireless mouse with adjustable DPI and long battery life.",
+        "images": [
+            "https://picsum.photos/id/237/200/300"
+        ],
+        "labelledPrice": 3500,
+        "price": 2500,
+        "stock": 120,
+        "isAvailable": true,
+        "__v": 0
+    },
+    {
+        "_id": "696aaa3ce3a59034e40a1353",
+        "productId": "PRD-002",
+        "name": "Mechanical Keyboard",
+        "altNames": [
+            "Gaming Keyboard",
+            "RGB Keyboard"
+        ],
+        "description": "Mechanical keyboard with RGB backlight and blue switches.",
+        "images": [
+            "https://example.com/images/keyboard1.png"
+        ],
+        "labelledPrice": 15500,
+        "price": 14000,
+        "stock": 45,
+        "isAvailable": true,
+        "__v": 0
+    }
+]
--- src/pages/admin/adminProduct.jsx
+++ src/pages/admin/adminProduct.jsx
@@ -1,8 +1,50 @@
+import { useEffect, useState } from "react"
+import { sampleProducts } from "../../assets/sampleProducts"
+import axios from "axios"
+
 export default function AdminProduct(){
+    const [products, setProducts] = useState(sampleProducts)
+    useEffect(
+        ()=>{
+            axios.get("http://localhost:5000/api/products").then(
+        (res)=>{
+            console.log(res.data)
+            setProducts(res.data)
+        }
+
+    )
+        },[]
+    );
+    
+
     return(
-        <div className="w-full h-full  border-8 border-black bg-amber-700  overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-300">
-            <div className="w-full h-[1500px]">
-            </div>
+        <div className="w-full h-full  border-8 border-black  overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-300">
+            <table className="w-full text-center  border border-black">
+                <thead>
+                    <tr>
+                        <th>Product ID</th>
+                        <th>Product Name</th>
+                        <th>Product Image</th>
+                        <th>Labeled Price</th>
+                        <th>Stock</th>
+                    </tr>
+                </thead>
+                <tbody className="border border-black">
+                    {products.map(
+                        (product,index)=>{
+                            return(
+                                <tr key={index} className="border border-black">
+                                    <td>{product.productId}</td>
+                                    <td>{product.name}</td>
+                                    <td><img src={product.images} alt="img" className="w-[100px] h-[100px]"/></td>
+                                    <td>{product.labeledPrice}</td>
+                                    <td>{product.stock}</td>
+                                </tr>
+                            )
+                        }
+                    )}
+                </tbody>
+            </table>
         </div>
     )
 }
